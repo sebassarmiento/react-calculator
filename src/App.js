c4736d45a0c496aba01082e83a5bf065efb1efed
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-12-15 15:34:23 -0500

    First upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      screen: 0
+    }
+  }
+
+  componentDidMount(){
+    document.body.onclick = this.handleClick.bind(this)
+  }
+
+  handleClick(e){
+    if(e.target.getAttribute('name') === 'number'){
+      this.setState({screen: (this.state.screen * 10) + parseFloat(e.target.innerHTML)})
+    }
+    if(e.target.getAttribute('name') === 'clear'){
+      this.setState({screen: 0})
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main-container">
+        <div className="screen" >
+          {this.state.screen}
+        </div>
+        <div className="board" >
+            <div name="clear" className="btn ac" >AC</div>
+            <div className="btn" >/</div>
+            <div className="btn" >X</div>
+            <div name="number" className="btn" >7</div>
+            <div name="number" className="btn" >8</div>
+            <div name="number" className="btn" >9</div>
+            <div className="btn" >-</div>
+            <div name="number" className="btn" >4</div>
+            <div name="number" className="btn" >5</div>
+            <div name="number" className="btn" >6</div>
+            <div className="btn" >+</div>
+            <div name="number" className="btn" >1</div>
+            <div name="number" className="btn" >2</div>
+            <div name="number" className="btn" >3</div>
+            <div className="btn result" >=</div>
+            <div name="number" className="btn zero" >0</div>
+            <div className="btn" >.</div>
+        </div>
       </div>
     );
   }
